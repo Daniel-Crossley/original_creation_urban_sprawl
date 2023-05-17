ad3eae0674b3b417d4ea6f15cea725c050c8c314
finn2011 <finnkelly1120@gmail.com>
2023-05-17 10:18:55 +1000

    semi colon

--- Spawn_conditions.m
+++ Spawn_conditions.m
@@ -1,10 +1,10 @@
 R = 400;          % number of rows
 C = 400;          % number of columns
 spawn = false(100,100); %setting spawn matrix
-spawn(:,:) = rand(100,100) > 0.5 %randomising spawnpoint
+spawn(:,:) = rand(100,100) > 0.5; %randomising spawnpoint
 
 A = ones(R,C);   % creating blank land for population
 
-A(151:250, 151:250) = spawn %seting spawnpoint to middle of land matrix
+A(151:250, 151:250) = spawn; %seting spawnpoint to middle of land matrix
 
 % TEst github 2
