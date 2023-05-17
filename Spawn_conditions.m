b7ed215c60cfcd6effd97bd764e6d765981e9d5c
Spyyoshi <133826023+Spyyoshi@users.noreply.github.com>
2023-05-17 10:08:08 +1000

    Add files via upload
    Here are the spaw conditions

---
+++ Spawn_conditions.m
@@ -0,0 +1,8 @@
+R = 400;          % number of rows
+C = 400;          % number of columns
+spawn = false(100,100); %setting spawn matrix
+spawn(:,:) = rand(100,100) > 0.5 %randomising spawnpoint
+
+A = ones(R,C);   % creating blank land for population
+
+A(151:250, 151:250) = spawn %seting spawnpoint to middle of land matrix
