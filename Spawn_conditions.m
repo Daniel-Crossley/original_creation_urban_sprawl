3de140e65ab66ba854de2555219a40e2fd2d2761
erinQUT <133825944+erinQUT@users.noreply.github.com>
2023-05-17 10:12:00 +1000

    added commet
    sajdfas

--- Spawn_conditions.m
+++ Spawn_conditions.m
@@ -6,3 +6,5 @@ spawn(:,:) = rand(100,100) > 0.5 %randomising spawnpoint
 A = ones(R,C);   % creating blank land for population
 
 A(151:250, 151:250) = spawn %seting spawnpoint to middle of land matrix
+
+% TEst github
