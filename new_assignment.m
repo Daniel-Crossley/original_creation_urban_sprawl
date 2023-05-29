d87543d84bf6ea53062529847ff5993b46a47284
erinQUT <133825944+erinQUT@users.noreply.github.com>
2023-05-29 14:44:43 +1000

    Update new_assignment.m
    I added calculating and plotting area. You can increase the steps I just needed some numerical value for steps

--- new_assignment.m
+++ new_assignment.m
@@ -50,9 +50,20 @@ set(gcf, 'KeyPressFcn', @KeyPressed) % this allows us to react to any key presse
 handle = imagesc(grid); % save the handle for when we want to update the image later
 title('Press any key to finish')
 drawnow
+
+%-----------------------------------
+% Define the fixed number of time steps
+num_steps = 100; % Adjust this value as needed
+
 %% Simulation 
+% Initialize variables for counting and area
+time_steps = zeros(1, num_steps);
+green_counts = zeros(1, num_steps);
+green_areas = zeros(1, num_steps);
 done = false;
-while ~done % See comments at the bottom of this file for an explanation of this "while" loop.
+step = 1;
+%--------------------------------------------
+while ~done && step <= num_steps% See comments at the bottom of this file for an explanation of this "while" loop.
             % For now, simply read this literally as saying we should take another step "while not done"
 
     live_neighbours = (grid(north, :) == 1) + (grid(north, west) == 1) + (grid(north, east) == 1) + (grid(:, west) == 1) + (grid(:, east) == 1) + (grid(south, west) == 1) + (grid(south, :) == 1) + (grid(south, east) == 1);
@@ -159,7 +170,52 @@ while ~done % See comments at the bottom of this file for an explanation of this
     resource_type = next_resource_type;
     imagesc(grid_colour); % this has the same effect as the line above, but is slower
     drawnow
+    
+    % -----------plotting area----------------
+
+    % Make sure that grid colours don't change when resources run out
+    grid_colour(grid == 0) = 0;
+    grid_colour(grid == 1) = 1;
+    grid_colour(grid == 2) = 2;
+    grid_colour(grid == 3) = 3;
+
+    resource_grid = next_resource_grid;
+    resource_type = next_resource_type;
+    imagesc(grid_colour);
+    drawnow
+    
+    % Calculate and graph area/count of green pixels
+    green_count = sum(grid(:) == 1);
+    green_area = green_count / (R * C);
+    
+    % Store count and area at each time step
+    time_steps(step) = step;
+    green_counts(step) = green_count;
+    green_areas(step) = green_area;
+    
+    step = step + 1; % Increment the step counter
 end
+
+% Trim the variables to the actual number of steps taken
+time_steps = time_steps(1:step-1);
+green_counts = green_counts(1:step-1);
+green_areas = green_areas(1:step-1);
+
+% Plot the count and area over time
+subplot(2, 1, 1);
+plot(time_steps, green_counts);
+xlabel('Time Step');
+ylabel('Green Pixel Count');
+title('Green Pixel Count over Time');
+
+subplot(2, 1, 2);
+plot(time_steps, green_areas);
+xlabel('Time Step');
+ylabel('Green Pixel Area');
+title('Green Pixel Area over Time');
+
+% ---------------plotting area-----------------------
+
 
 function KeyPressed(~, ~)
     % This function is called by MATLAB automatically, whenever the user presses a key in the Figure window.
