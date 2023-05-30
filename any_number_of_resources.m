950f64272ba87d0e3389ff9b1623304cc6120e74
caleb-borg <n11608757@qut.edu.au>
2023-05-30 19:11:23 +1000

    Update any_number_of_resources.m

--- any_number_of_resources.m
+++ any_number_of_resources.m
@@ -142,10 +142,6 @@ while ~done % See comments at the bottom of this file for an explanation of this
     resource_type = next_resource_type;
 
     grid_gaussian = imgaussfilt(grid,3);
-    % Make sure that grid colours don't change when resources run out
-    %for i=0:3
-    %    grid_colour(grid == i) = i;
-    %end
 
     imagesc(grid_gaussian, [0 1.5]); % Display the coloured grid
     drawnow
