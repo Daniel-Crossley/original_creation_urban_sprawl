c96bb157ab3934bbcbe462eb2e3f3ee683d229e9
caleb-borg <n11608757@qut.edu.au>
2023-05-30 14:00:50 +1000

    Update any_number_of_resources.m

--- any_number_of_resources.m
+++ any_number_of_resources.m
@@ -1,24 +1,28 @@
 R = 400;          % number of rows
 C = 400;          % number of columns
 
+
+% Initialise matrices
 grid = zeros(R,C);
 grid_colour = zeros(R,C);
 neighbouring_resource = zeros(R,C);
 resource_type = zeros(R, C);
+live_neighbours = zeros(R,C);
+resource_neighbours = zeros(R,C);
+resource_grid = zeros(R,C);
 
+% Setting 
 spawn = false(200,200); %setting spawn matrix
 spawn(:,:) = rand(200,200) < 0.2; %randomising spawnpoint
-
 grid(101:300,101:300) = spawn;
-next_grid = grid;
 
-number_of_resources = 5;
+% Number of resource nodes to place on the grid
+number_of_resources = 6;
 
+% Initialising cell array to contain matrix for each resource
 resource_cell_array = cell(number_of_resources,1);
 placement_x = randi(R,1, number_of_resources);
 placement_y = randi(C,1, number_of_resources);
-
-resource_grid = zeros(R,C);
 
 for i=1:number_of_resources
     grid(placement_x(i), placement_y(i)) = 3;
@@ -26,16 +30,15 @@ for i=1:number_of_resources
     resource_grid(placement_x(i), placement_y(i)) = 10000;
 end
 
+% Defining the next grid iteration to match the initial grid configuration before being edited within the while loop
+next_grid = grid;
+next_resource_grid = resource_grid;
 next_resource_type = resource_type;
-
-
-live_neighbours = zeros(R,C);
-resource_neighbours = zeros(R,C);
 
 north = [R 1:R-1];     % indices of north neighbour
 east  = [2:C 1];       % indices of east neighbour
 south = [2:R 1];       % indices of south neighbour
-west  = [C 1:C-1];
+west  = [C 1:C-1];     % indices of west neighbour
 
 % Show the initial frame in the animation
 set(figure, 'Visible', 'on', 'Position', get(0,'Screensize'))
@@ -47,9 +50,15 @@ drawnow
 done = false;
 while ~done % See comments at the bottom of this file for an explanation of this "while" loop.
             % For now, simply read this literally as saying we should take another step "while not done"
-
+    
+    % Initiate neighbouring grids for the iteration
+    % Count number of live cells neighbouring each cell
     live_neighbours = (grid(north, :) == 1) + (grid(north, west) == 1) + (grid(north, east) == 1) + (grid(:, west) == 1) + (grid(:, east) == 1) + (grid(south, west) == 1) + (grid(south, :) == 1) + (grid(south, east) == 1);
+    
+    % Count number of resource or resource neighbouring cells that neighbour each cell
     resource_neighbours = (grid(north, :) > 1) + (grid(north, west) > 1) + (grid(north, east) > 1) + (grid(:, west) > 1) + (grid(:, east) > 1) + (grid(south, west) > 1) + (grid(south, :) > 1) + (grid(south, east) > 1);        
+    
+    % Count number of resource neighbouring cells that neighbour each cell
     live_resource_neighbours = (grid(north, :) == 2) + (grid(north, west) == 2) + (grid(north, east) == 2) + (grid(:, west) == 2) + (grid(:, east) == 2) + (grid(south, west) == 2) + (grid(south, :) == 2) + (grid(south, east) == 2);
 
     for i=1:number_of_resources 
@@ -66,8 +75,11 @@ while ~done % See comments at the bottom of this file for an explanation of this
             % Neighbouring resource cell rules
             if (grid(i,j) == 2)
                 if resource_type(i,j) == 0 % If no resource type, run standard game of life
-                    next_grid(i,j) = 1;
-                    resource_neighbours = (next_grid(north, :) > 1) + (next_grid(north, west) > 1) + (next_grid(north, east) > 1) + (next_grid(:, west) > 1) + (next_grid(:, east) > 1) + (next_grid(south, west) > 1) + (next_grid(south, :) > 1) + (next_grid(south, east) > 1);       
+                    grid(i,j) = 1;
+                    
+                    % Update the neighbouring grids for the rest of the grid simulation iteration
+                    live_resource_neighbours = (grid(north, :) == 2) + (grid(north, west) == 2) + (grid(north, east) == 2) + (grid(:, west) == 2) + (grid(:, east) == 2) + (grid(south, west) == 2) + (grid(south, :) == 2) + (grid(south, east) == 2);
+                    resource_neighbours = (grid(north, :) > 1) + (grid(north, west) > 1) + (grid(north, east) > 1) + (grid(:, west) > 1) + (grid(:, east) > 1) + (grid(south, west) > 1) + (grid(south, :) > 1) + (grid(south, east) > 1);       
                 else % There is a resource neighbour - becomes a resource neigbour itself
                     next_grid(i,j) = 2;
                 end
@@ -85,9 +97,9 @@ while ~done % See comments at the bottom of this file for an explanation of this
             elseif grid(i,j) == 1
                 if resource_neighbours(i,j) == 0
                     if live_neighbours(i,j) == 3 || live_neighbours(i,j) == 2
-                        next_grid(i, j) = 1;
+                        next_grid(i,j) = 1;
                     else
-                        next_grid(i, j) = 0;
+                        next_grid(i,j) = 0;
                     end
                 else
                     next_grid(i,j) = 2;
@@ -119,25 +131,22 @@ while ~done % See comments at the bottom of this file for an explanation of this
                     next_grid(i,j) = 3;
                 end
             end
-                
-    
-            % Else if cell is resource, other ruleset
-            % Else if cell is live and touching resource
-            % Else cell is dead; standard ruleset
         end
     end
     
+    next_grid((next_grid == 2) & (next_resource_type == 0)) = 1; % Final check to ensure all cells no longer connected to a resource are turned into live cells again
+
+    % Updating matrices
     grid = next_grid;
+    resource_grid = next_resource_grid; 
+    resource_type = next_resource_type;
 
     % Make sure that grid colours don't change when resources run out
-    grid_colour(grid == 0) = 0;
-    grid_colour(grid == 1) = 1;
-    grid_colour(grid == 2) = 2;
-    grid_colour(grid == 3) = 3;
+    for i=0:3
+        grid_colour(grid == i) = i;
+    end
 
-    resource_grid = next_resource_grid;
-    resource_type = next_resource_type;
-    imagesc(grid_colour); % this has the same effect as the line above, but is slower
+    imagesc(grid_colour); % Display the coloured grid
     drawnow
 end
 
